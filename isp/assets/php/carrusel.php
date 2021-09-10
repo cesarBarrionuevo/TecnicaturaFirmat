a9a711137bdc20e412e698cc8bae78c526fd9d96
unknown <mateue14@gamil.com>
2021-09-10 19:20:26 -0300

    Arreglando errores

---
+++ isp/assets/php/carrusel.php
@@ -0,0 +1,68 @@
+
+
+<div class="container pt-1 "   >
+    
+    
+
+    <div class="row justify-content-center     ">
+
+
+        <div class="bd-example mt-5 shadow p-3 mb-5 bg-white rounded  " style="border: 1px solid black" >
+            <div id="carouselExampleCaptions" class="carousel slide   "  data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+                    
+        
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100 imagenCarrusel" data-src="holder.js/600x300?auto=yes&amp;bg=777&amp;fg=555&amp;text=First slide" alt="First slide [900x300]" src="assets/img/ispManuelLeivaCarruselImg1.jpg" data-holder-rendered="true">
+                        <div class="carousel-caption "> 
+                            <a href="#" id="textoCarrusel" >¡Conoce todos nuestas propuestas de estudio!</a>
+                                  
+                        
+                        </div>
+                    </div>
+                    <div class="carousel-item ">
+                        <img class="d-block w-100 imagenCarrusel" data-src="holder.js/600x300?auto=yes&amp;bg=777&amp;fg=555&amp;text=First slide" alt="First slide [900x300]" src="assets/img/graduados.jpg" data-holder-rendered="true">
+                        <div class="carousel-caption ">  
+                            <a href="#" id="textoCarrusel" >¿Queres estudiar programación?</a>
+                                         
+                            
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 imagenCarrusel " data-src="holder.js/600x300?auto=yes&amp;bg=777&amp;fg=555&amp;text=First slide" alt="First slide [900x300]" src="assets/img/ispManuelLeivaCarruselBibliotecaImg3.jpg" data-holder-rendered="true">
+                        <div class="carousel-caption  ">                    
+                                <a href="#" id="textoCarrusel" >¡Conoce todos los libros disponibles de nuestra biblioteca! </a>
+                                
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img class="d-block w-100 imagenCarrusel " data-src="holder.js/600x300?auto=yes&amp;bg=777&amp;fg=555&amp;text=First slide" alt="First slide [900x300]" src="assets/img/ispManuelLeivaCarruselCampusImg2.jpg" data-holder-rendered="true">
+                        <div class="carousel-caption  ">                    
+                                <a href="#" id="textoCarrusel" >¡Conoce todos los libros disponibles de nuestra biblioteca! </a>
+                                
+                        </div>
+                    </div>
+                
+                </div>
+                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+
+                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+            </div>
+        </div>
+        
+    
+    </div>
+</div>
---
+++ isp/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>MauelLeiva</title>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/mediaQueries.css"> 
+        <link rel="stylesheet" href="assets/css/styles.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+        
+    </head>
+    <body  style="margin: 50px 0px 0px 0px;  background: linear-gradient(#3f0948 40%, #a61ce0 60%);">
+        <header>
+            <nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark navbar--apple">
+                <div class="container">
+                    <button data-toggle="collapse" class="navbar-toggler" data-target="#menu">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="navbar-toggler-icon"><i class="la la-navicon"></i></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="menu">
+                        <ul class="navbar-nav flex-grow-1 justify-content-between">
+                            <li class="nav-item d-none d-xs-block d-md-block">
+                                <a class="nav-link" href="#"><i class="fa fa-mortar-board apple-logo"></i>&nbsp; Instituto Superior "Manuel Leiva"</a>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="#">Carreras</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Docentes</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Alumnado</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Campus Virtual</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+      
+        <div class="row" style="height: 40px;background: rgba(249,185,88,0.91) ;">
+            <div class="col d-xl-flex justify-content-xl-center align-items-xl-center" data-bss-hover-animate="pulse" style="height: 40px;">
+                <a class="text-dark miLink" href="#">Nuestra Institución</a>
+            </div>
+            <div class="col d-xl-flex justify-content-xl-center align-items-xl-center" data-bss-hover-animate="pulse" style="height: 40px;">
+                <a class="text-dark miLink" href="#">Capacitaciones</a>
+            </div>
+            <!--
+            <div class="col d-xl-flex justify-content-xl-center align-items-xl-center" style="height: 140px;background: rgba(86,80,80,0.72);">
+                <img class="img-fluid" src="assets/img/LogoLeiva.png" style="width: 120px;">
+            </div>
+            -->
+            <div class="col d-xl-flex justify-content-xl-center align-items-xl-center" style="height: 40px;">
+                <a class="text-dark miLink" data-bss-hover-animate="pulse" href="#">Normativa</a>
+            </div>
+            <div class="col d-xl-flex justify-content-xl-center align-items-xl-center" style="height: 40px;">
+                <a class="text-dark miLink" data-bss-hover-animate="pulse" href="#">Biblioteca</a>
+            </div>
+        </div>
+
+         
+      
+
+        
+        
+
+        <?php
+
+        require_once ("/xampp/htdocs/ProyectoFirmat/TecnicaturaFirmat/isp/assets/php/carrusel.php");
+
+     ?>
+	    <div style="margin: 200px;">
+            <div class="card-group">
+                <div class="card" style="background: rgba(255,255,255,0);border-color: rgba(33,37,41,0);">
+                    <img class="card-img-top w-100 d-block" src="assets/img/estatua.jpg">
+                    <div class="card-body">
+                        <h4 class="card-title">Noticias</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                        <button class="btn btn-info d-xl-flex" type="button">ver más&nbsp;<i class="fa fa-arrow-circle-right d-xl-flex justify-content-xl-center align-items-xl-center"></i></button>
+                    </div>
+                </div>
+                <div class="card" style="background: rgba(255,255,255,0);border-color: rgba(33,37,41,0);">
+                    <img class="card-img-top w-100 d-block" src="assets/img/estudiante.jpg">
+                    <div class="card-body"><h4 class="card-title">Destacados</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                        <button class="btn btn-info d-xl-flex" type="button">ver más&nbsp;<i class="fa fa-arrow-circle-right d-xl-flex justify-content-xl-center align-items-xl-center"></i></button>
+                    </div>
+                </div>
+                <div class="card" style="background: rgba(255,255,255,0);border-color: rgba(33,37,41,0);">
+                    <img class="card-img-top w-100 d-block" src="assets/img/examen.jpg">
+                    <div class="card-body"><h4 class="card-title">Mesas de Examen</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                        <button class="btn btn-info d-xl-flex" type="button">ver más&nbsp;<i class="fa fa-arrow-circle-right d-xl-flex justify-content-xl-center align-items-xl-center"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-dark">
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>Servicios</h3>
+                            <ul>
+                                <li>
+                                    <a href="#">Carreras</a>
+                                </li>
+                                <li>
+                                    <a href="#">Docentes</a>
+                                </li>
+                                <li>
+                                    <a href="#">Alumnado</a>
+                                </li>
+                                <li>
+                                    <a href="#">Campus Virtual</a>
+                                </li>
+                                <li>
+                                    <a href="#">Biblioteca</a>
+                                </li>
+                                <li>
+                                    <a href="#">Capacitaciones</a>
+                                </li>
+                                <li>
+                                    <a href="#">Normativa</a>
+                                </li>
+                                <li>
+
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6 col-md-3 col-xl-4 item">
+                            <h3>Contactos</h3>
+                            <ul>
+                                <li>
+                                    <a href="mailto:estudiantesprofesorado1@gmail.com">
+                                        <strong>estudiantesprofesorado1@gmail.com</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#"><strong>ingresantesprofesorado1@gmail.com</strong></a>
+                                </li>
+                                <li>
+                                    <a href="#"><strong>tecnicatura01software01firmat@gmail.com&nbsp;</strong></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 col-xl-5 item text">
+                            <h3>Instituto Superior N° 1 "Manuel Leiva"</h3>
+                            <p>Dante Alighieri 2385&nbsp;</p>
+                            <ul>
+                                <li>Casilda - Santa Fe - Argentina</li>
+                                <li>Teléfonos (03464) 422333</li>
+                            </ul>
+                        </div>
+                        <div class="col item social">
+                            <a href="#"><i class="icon ion-social-facebook"></i></a>
+                            <a href="#"><i class="icon ion-social-twitter"></i></a>
+                            <a href="#"><i class="icon ion-social-github"></i></a>
+                            <a href="#"><i class="icon ion-social-instagram"></i></a>
+                        </div>
+                    </div>
+                    <p class="copyright">TDS. © DesarrolloWeb 2021</p>
+                </div>
+            </footer>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/script.min.js"></script>
+    </body>
+</html>
